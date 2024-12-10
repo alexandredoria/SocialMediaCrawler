2d889a67543e9ccda765ed224a585d24bb65cae0
Alexandre Dória <alexandredoria.01@hotmail.com>
2024-12-10 14:18:09 -0300

    refactor: simplify expected test assertion

--- src/Command/UpdateSocialMediaUserCommand.php
+++ src/Command/UpdateSocialMediaUserCommand.php
@@ -7,7 +7,6 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'app:update-all-social-media-users',
@@ -26,21 +25,15 @@ class UpdateSocialMediaUserCommand extends Command
         $this->dispatcher = $dispatcher;
     }
 
+    /**
+     * @SuppressWarnings("unused")
+     */
     public function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->dispatcher->updateAllUsers();
 
-        $io = $this->createSymfonyStyle($input, $output);
-        $io->title("Dispatched a message to update all users data");
+        echo "Dispatched a message to update all users data" . PHP_EOL;
 
         return Command::SUCCESS;
-    }
-
-    /**
-     * @infection-ignore-all
-     **/
-    protected function createSymfonyStyle(InputInterface $input, OutputInterface $output): SymfonyStyle
-    {
-        return new SymfonyStyle($input, $output);
     }
 }
